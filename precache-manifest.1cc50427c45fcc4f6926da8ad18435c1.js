21b859900f2e20ac86c3709fa463f4ca7d48cf19
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2020-10-16 18:18:36 -0400

    Updates

---
+++ precache-manifest.1cc50427c45fcc4f6926da8ad18435c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f3cfa74c6f10df6e15dc3e3ade969d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "37423b9d1b7c026c131d",
+    "url": "/monsters-rolodex/static/css/main.07c19066.chunk.css"
+  },
+  {
+    "revision": "2cee125e2d4aaeb763df",
+    "url": "/monsters-rolodex/static/js/2.216c1404.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.216c1404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37423b9d1b7c026c131d",
+    "url": "/monsters-rolodex/static/js/main.897726a1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
